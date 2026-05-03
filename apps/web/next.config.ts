e8654f2168af62d77b9b0d3b5c01d07ad74da1cf
rogeriosaj <rogeriosucuglia@gmail.com>
2026-05-02 23:59:21 -0300

    chore: setup github actions for pages deployment

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/ajwings" : "",
   typedRoutes: true,
   reactCompiler: true,
   images: {
